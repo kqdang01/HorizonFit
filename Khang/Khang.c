3e5363c555e3d17f471f5fc054e0a71e55a753aa
kqdang01 <113054344+kqdang01@users.noreply.github.com>
2025-03-21 19:35:03 -0500

    create demo of initializing user accounts
    implemented the cJSON lib in order to format user data to json file

--- Khang/Khang.c
+++ Khang/Khang.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "cJSON.h"
+
+#define MAX_LINE 30
+
+cJSON* createUserObj(cJSON*,char*,char*);
 
 int main(void)
 {
+    char name[MAX_LINE] = {};
+    char pass[MAX_LINE] = {};
 
+    //opening the database file for reading and writing
+    FILE *db = fopen("db.json","r+");
+    if (db == NULL)
+    {
+        printf("File did not open correctly, exiting...\n");
+        exit(0);
+    }
+    //prompt for name and pass for creating an account
+    printf("Enter a username: ");
+    scanf("%s", name);
+    printf("\n");
+    getchar();
+    printf("Enter a password: ");
+    scanf("%s", pass);
+    printf("\n");
+    cJSON *user = cJSON_CreateObject();
+    user = createUserObj(user,name,pass);
+    printf("%s\n", cJSON_Print(user));
+    fclose(db);
     return 0;
+}
+
+//creating json user object
+cJSON* createUserObj(cJSON* acc,char* username, char* password)
+{
+    cJSON_AddStringToObject(acc, "username", username);
+    cJSON_AddStringToObject(acc, "password", password);
+    return acc;
 }
