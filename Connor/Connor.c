64d4f9e8ca4de24cffa2109552f0726ca240ae78
Connor Graham <clg4516@mavs.uta.edu>
2025-04-07 10:48:19 -0500

    case

--- Connor/Connor.c
+++ Connor/Connor.c
@@ -1,88 +1,229 @@
+
+// use link list and case staments
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
  
-#define RUNNING_CALORIES_PER_MINUTE 11.7
+
 //cardio
 #define TREADMIL_CALORUES 11.7
 #define ELLIPTICAL_CALORUES 3
 #define ROWER_CALORUES 4
 #define UPERIGHT_BIKE_CALORUES 8
-#define STEPMILL_CALORUES
-#define RECUMBENT_STEPPER_CALORUES
-#define RECUBENT_BIKE_CALORUES
-#define ARC_CALORUES
-#define UBE_CALORUES
+#define STEPMILL_CALORUES 8
+#define RECUMBENT_STEPPER_CALORUES 7.2
+#define RECUBENT_BIKE_CALORUES 8.67
+#define ARC_CALORUES 10.8
+#define UBE_CALORUES 9
 
 //Strength
-#define CABLE_TOWER_BASE_CALORUES
-#define FITBENCH_BASE_CALORUES
-#define FREE_WIGHTS_BASE_CALORUES
-#define PULLEY_BASE_CALORUES
-#define SMITH_MACHINE_BASE_CALORUES
-#define FUNCTIONAL_STATION_BASE_CALORUES
-#define TRX_BASE_CALORUES
-#define SQUAT_LUNGE_MACHINE_BASE_CALORUES
-#define SUPINE_BENCH_PRESS_BASE_CALORUES
-#define GLUTE_MACHINE_BASE_CALORUES
+#define CABLE_TOWER_BASE_CALORUES 15
+#define FITBENCH_BASE_CALORUES 8.5
+#define FREE_WEIGHTS_BASE_CALORUES 4
+#define PULLEY_BASE_CALORUES 7
+#define SMITH_MACHINE_BASE_CALORUES 4
+#define FUNCTIONAL_STATION_BASE_CALORUES 4
+#define TRX_BASE_CALORUES 6.67
+#define SQUAT_LUNGE_MACHINE_BASE_CALORUES 8
+#define SUPINE_BENCH_PRESS_BASE_CALORUES 4
+#define GLUTE_MACHINE_BASE_CALORUES 5
 
 
-void cc()
+void CC()
+    {
+        int TC=0;
+        char UE;
+        
+        printf ("Did you use any equipment? (y/n): ");
+        scanf("%c", &UE);
+
+        while (UE=='y'|| UE=='Y')
+        {
+            int equipment;
+            int ET,duration=0,sets=0,reps=0,CPM=0,EC=0,SC;
+            char EN[100];
+
+            printf("\n===== EQUIPMENT TYPE ======\n");
+            printf("1. Cardio Equipment\n");
+            printf("2. Strength Equipment\n");
+            printf("Select equipment type (1-2): ");
+            scanf("%d", &ET);
+
+            switch (ET)
+            {
+                case 1:
+                printf("\n====== CARDIO EQUIPMENT ======\n");
+                printf("1. Teadmill\n");
+                printf("2. Elliptical\n");
+                printf("3. Rower\n");
+                printf("4. Upright Bike\n");
+                printf("5. Stepmill\n");
+                printf("6. Recumbent Bike\n");
+                printf("7. Recumbent Stepper\n");
+                printf("8. Arc\n");
+                printf("9. UBE\n");
+                printf("Select Cardio equipment (1-9): ");
+                scanf("%d", &equipment);
+
+                printf("Enter the number of minutes used: ");
+                scanf("%d", &duration);
+                //if(equipment == 1)
+                // {
+                //     CPM=TREADMIL_CALORUES;
+                // }
+                // else if(equipment ==2)
+                // [
+                //     CPM=ELLIPTICAL_CALORUES;
+                // ]
+
+                switch (equipment)
+                {
+                    case 1:
+                        strcpy(EN, "Treadmill");
+                        CPM=TREADMIL_CALORUES;
+                        break;
+                    case 2:
+                        strcpy(EN, "Elliptical");
+                        CPM=ELLIPTICAL_CALORUES;
+                        break;
+                    case 3:
+                        strcpy(EN, "Rower");
+                        CPM=ROWER_CALORUES;
+                        break;
+                    case 4:
+                        strcpy(EN, "Upright Bike");
+                        CPM=UPERIGHT_BIKE_CALORUES;
+                        break;
+                    case 5:
+                        strcpy(EN, "Stepmill");
+                        CPM=STEPMILL_CALORUES;
+                        break;
+                    case 6:
+                        strcpy(EN, "Recumbent Bike");
+                        CPM=RECUBENT_BIKE_CALORUES;
+                        break;
+                    case 7:
+                        strcpy(EN, "Recumbent Stepper");
+                        CPM=RECUMBENT_STEPPER_CALORUES;
+                        break;
+                    case 8:
+                        strcpy(EN, "Arc");
+                        CPM=ARC_CALORUES;
+                        break;
+                    case 9:
+                        strcpy(EN, "UBE");
+                        CPM=UBE_CALORUES;
+                        break;
+                    default:
+                        printf("Invalid choice.\n");
+                        strcpy(EN, "Unknoen Cardio");
+                        CPM=5;
+                }
+                EC=duration * CPM;
+                printf("%s: %d minutes - %d calorues\n", EN,duration,EC);
+                break;
+
+                case 2:
+                printf("\n====== STRENGTH EQUIPMENT ======\n");
+                printf("1. Cable Tower\n");
+                printf("2. Fit Bench\n");
+                printf("3. Free Weights\n");
+                printf("4. Pulley\n");
+                printf("5. Smith Machine\n");
+                printf("6. Functional Station\n");
+                printf("7. TRX\n");
+                printf("8. Supine Bench Press\n");
+                printf("9. Squat Lunge Machine\n");
+                printf("10. Glute Machine\n");
+                printf("Select Cardio equipment (1-10): ");
+                scanf("%d", &SC);
+
+                printf("Enter the number of minutes used: ");
+                scanf("%d", &duration);
+                printf("Enter the number of sets: ");
+                scanf("%d", &sets);
+                printf("Enter the number of reps per set: ");
+                scanf("%d", &reps);
+
+                switch (SC)
+                {
+                    case 1:
+                        strcpy(EN, "Cable Tower");
+                        CPM=CABLE_TOWER_BASE_CALORUES;
+                        break;
+                    case 2:
+                        strcpy(EN, "Fit Bench");
+                        CPM=FITBENCH_BASE_CALORUES;
+                        break;
+                    case 3:
+                        strcpy(EN, "Free Weights");
+                        CPM=FREE_WEIGHTS_BASE_CALORUES;
+                        break;
+                    case 4:
+                        strcpy(EN, "Pulley");
+                        CPM=PULLEY_BASE_CALORUES;
+                        break;
+                    case 5:
+                        strcpy(EN, "Smith Machine");
+                        CPM=SMITH_MACHINE_BASE_CALORUES;
+                        break;
+                    case 6:
+                        strcpy(EN, "Functional Station");
+                        CPM=FUNCTIONAL_STATION_BASE_CALORUES;
+                        break;
+                    case 7:
+                        strcpy(EN, "TRX");
+                        CPM=TRX_BASE_CALORUES;
+                        break;
+                    case 8:
+                        strcpy(EN, "Supine Bench Press");
+                        CPM=SUPINE_BENCH_PRESS_BASE_CALORUES;
+                        break;
+                    case 9:
+                        strcpy(EN, "Squat Lunge Machine");
+                        CPM=SQUAT_LUNGE_MACHINE_BASE_CALORUES;
+                        break;
+                    case 10:
+                        strcpy(EN, "Glute Machin");
+                        CPM=GLUTE_MACHINE_BASE_CALORUES;
+                    default:
+                        printf("Invalid choice.\n");
+                        strcpy(EN, "Unknown Cardio");
+                        CPM=5;
+            }
+            CPM +=(sets * reps)/10;
+            EC=duration * CPM;
+            printf("%s: %d minutes (%d sets, %d reps) -%d calories\n", EN,duration,sets,reps,EC);
+            break;
+
+            default:
+            printf("Invalid selection. Please try again.\n");
+            continue;
+
+           
+        }
+        TC +=EC;
+            printf("Did you use another equipment? (y/n): ");
+            scanf("%c", &UE);
+    }
+    printf("\n======== WORKOUT SUMMARY =======\n");
+    printf("TOTAL CALORIES BURNED: %d Kcal\n", TC);
+    printf("===============================\n");
+
+}
+int main()
 {
-    int runTime, eTime, totalcal =0;
-    char useE;
-//run time
-    printf("Enter the number of minutes you ran for: ");
-    scanf("%d",&runTime);
-    totalcal += runTime * RUNNING_CALORIES_PER_MINUTE;
-
-    //asking if they used any equipment
-    printf("Did you use any equipment? (y/n): ");
-    scanf("%c", &useE);
-
-    while (useE =='y' || useE == 'y')
-    {
-        char eType[30];
-        int sets=0, rps=0;
-
-        printf("Enter the type of equipment used: ");
-        scanf("%s",&eType);
-        printf("Enter the number of minutes used the equipment: ");
-        scanf("%s",&eTime);
-
-        int cpm = ;
-
-        //cardio
-    if (strcmp(eType,"Treadmill")==0)
-    {
-        cpm=TREADMIL_CALORUES;
-    }else if (srtcmp(eType,"Elliptical")==0)
-    {
-        cpm=ELLIPTICAL_CALORUES;
-    }else if (srtcmp(eType,"Rower")==0)
-    {
-        cpm=ROWER_CALORUES;
-    }else if (srtcmp(eType,"UprightBike")==0)
-    {
-        cpm=UPERIGHT_BIKE_CALORUES;
-    }else if (srtcmp(eType,"Stepmill")==0)
-    {
-        cpm=STEPMILL_CALORUES;
-    }else if (srtcmp(eType,"RecumbentBike")==0)
-    {
-        cpm=RECUBENT_BIKE_CALORUES;
-    }else if (srtcmp(eType,"RecumbentStepper")==0)
-    {
-        cpm=RECUMBENT_STEPPER_CALORUES;
-    }else if (srtcmp(eType,"Arc")==0)
-    {
-        cpm=ARC_CALORUES;
-    }else if (srtcmp(eType,"UBE")==0)
-    {
-        cpm=UBE_CALORUES;
-    }
-
-    //strength equipment
-    
-    }
+    CC();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
